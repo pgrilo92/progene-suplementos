0e1ec99c91016d26685c19f4e95fbe9b4432845a
Joaquim Grilo <pgrilo92@hotmail.com>
2018-11-12 17:44:59 +0000

    heroku push pt 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'peaceful-garden-91388.herokuapp.com', :protocol => 'https' }
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
